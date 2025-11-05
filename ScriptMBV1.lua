150f371fcde29690b5a4400ea2df636ee0a3144b
trantruongmanhbao-ops <trantruongmanhbao@gmail.com>
2025-11-05 20:40:33 +0700

    Add files via upload

---
+++ ScriptMBV1.lua
@@ -0,0 +1,6 @@
+Config = {
+    namescript = "Manhbao Hub",--Tên Script
+    logoscript = "125181362377056",--ID Logo Script
+    tacgia = "Tran Truong Manh Bao"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tngaming123/vesion/refs/heads/main/scriptbffree.lua"))()
